c78e5c770f5087f941d350fc0a4433e37b46738b
Casey Haber <haber.casey@gmail.com>
2024-12-14 11:24:33 -0800

    feat: Update API to read CSV file using new read method

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,10 +1,7 @@
-   import { promises as fs } from 'fs';
-   import path from 'path';
+   // src/routes/api/+server.js
+   import { read } from "$app/server";
+   import somefile from './test.csv';
 
    export const GET = async () => {
-       const csvPath = path.resolve('src/lib/data/test.csv');
-       const csvData = await fs.readFile(csvPath, 'utf-8');
-       return new Response(csvData, {
-           headers: { 'Content-Type': 'text/csv' }
-       });
+       return await read(somefile).text();
    };
