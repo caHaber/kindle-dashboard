715d91873556d5810e2b1193ece9018efaabd4b4
Casey Haber <haber.casey@gmail.com>
2024-12-14 12:46:49 -0800

    style: Update navigation bar color and background image path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,12 +20,10 @@
 
 <div class="flex h-screen">
 	<!-- Navigation Bar -->
-	<nav class="w-1/4 bg-gradient-to-b from-green-200 to-green-400 p-4 text-white">
+	<nav class="w-1/4 bg-gradient-to-b from-blue-600 to-blue-100 p-4 text-white">
 		<h3 class="mb-4 text-2xl">Navigation</h3>
 		<ul>
 			<li class="mb-2"><a href="#" class="block rounded-lg p-2 hover:underline">Home</a></li>
-			<li class="mb-2"><a href="#" class="block rounded-lg p-2 hover:underline">Dashboard</a></li>
-			<li class="mb-2"><a href="#" class="block rounded-lg p-2 hover:underline">Settings</a></li>
 		</ul>
 	</nav>
 
@@ -56,6 +54,6 @@
 	}
 	.right-panel {
 		background-size: cover;
-		background-image: url('/src/lib/assets/background.svg');
+		background-image: url('/images/background.svg?url');
 	}
 </style>
