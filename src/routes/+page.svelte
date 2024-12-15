10f8023c848c3b46ee35ad43d9ad6fce533a31ae
Casey Haber <haber.casey@gmail.com>
2024-12-14 16:36:06 -0800

    style: Improve layout and styling of the dashboard components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,10 +25,15 @@
 			<li class="mb-4">
 				<a
 					href="#"
-					class="flex items-center block rounded-md bg-gray-700 p-2 transition duration-300 ease-in-out hover:bg-gray-600"
+					class="block flex items-center rounded-md bg-gray-700 p-2 transition duration-300 ease-in-out hover:bg-gray-600"
 				>
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
-						<path d="M11 2v20c-5-1-9-6-9-11s4-10 9-11zm2 0c5 1 9 6 9 11s-4 10-9 11v-20z"/>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="mr-2 h-5 w-5"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+					>
+						<path d="M11 2v20c-5-1-9-6-9-11s4-10 9-11zm2 0c5 1 9 6 9 11s-4 10-9 11v-20z" />
 					</svg>
 					Home
 				</a>
@@ -38,8 +43,22 @@
 
 	<!-- Main Content -->
 	<div class="right-panel w-4/5 p-4">
-		<h3 class="mb-6 text-3xl font-bold text-gray-800">Kindle Dashboard</h3>
-		<div class="chart-container rounded-lg bg-white p-6 shadow-md">
+		<h3 class="w-1/1 mb-6 text-3xl font-bold text-gray-800">Kindle Dashboard</h3>
+		<div class="h-2/3 w-1/2 rounded-lg bg-white p-6 shadow-md">
+			<LayerCake
+				{data}
+				x="x"
+				y="y"
+				padding={{ bottom: 20, left: 35 }}
+				yScale={scaleBand().paddingInner(0.05)}
+				xDomain={[0, null]}
+			>
+				<Svg>
+					<Bar fill={'lightblue'} />
+				</Svg>
+			</LayerCake>
+		</div>
+		<div class="h-2/3 w-1/2 rounded-lg bg-white p-6 shadow-md">
 			<LayerCake
 				{data}
 				x="x"
