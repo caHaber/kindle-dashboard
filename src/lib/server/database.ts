2b393b37736ac31fb708ddcad68fbf935d9a6f0b
Casey Haber <haber.casey@gmail.com>
2024-12-15 19:22:54 -0800

    Try with normal

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,6 +1,7 @@
 // import { DuckDBInstance } from '@duckdb/node-api';
 import genericPool from 'generic-pool'
 import { MOTHERDUCK_TOKEN } from '$env/static/private'
+import duckdb from 'duckdb-async'
 
 
 const pool = genericPool.createPool(
@@ -9,34 +10,38 @@ const pool = genericPool.createPool(
 
             let _query: Promise<(query: string) => any>
 
-            _query = import("duckdb-async")
-                .then(duckdb => duckdb.Database)
-                .then(Database => Database.create('md:kindle-data', {
-                    'motherduck_token': MOTHERDUCK_TOKEN,
-                }))
-                .then((db: any) => ((query: string) => db.all(`SET home_directory='/tmp'; ${query}`)))
-                .catch(async error => {
-                    console.log("duckdb init error:", error)
-                    //@ts-expect-error error
-                    let duckdb = await import("duckdb-lambda-x86");
-                    let Database: any = await duckdb.Database;
-                    const db = new Database('md:kindle-data', {
-                        'motherduck_token': MOTHERDUCK_TOKEN,
-                    })
-                    const connection = db.connect()
-                    return (query: string) => {
-                        return new Promise((resolve, reject) => {
-                            connection.all(`SET home_directory='/tmp'; ${query}`, (err: any, res: any) => {
-                                if (err) reject(err);
-                                resolve(res);
-                            })
-                        })
-                    }
-                })
+            const db = await duckdb.Database.create('md:kindle-data', {
+                'motherduck_token': MOTHERDUCK_TOKEN,
+            })
+
+            // _query = import("duckdb-async")
+            //     .then(duckdb => duckdb.Database)
+            //     .then(Database => Database.create('md:kindle-data', {
+            //         'motherduck_token': MOTHERDUCK_TOKEN,
+            //     }))
+            //     .then((db: any) => ((query: string) => db.all(`SET home_directory='/tmp'; ${query}`)))
+            //     .catch(async error => {
+            //         console.log("duckdb init error:", error)
+            //         //@ts-expect-error error
+            //         let duckdb = await import("duckdb-lambda-x86");
+            //         let Database: any = await duckdb.Database;
+            //         const db = new Database('md:kindle-data', {
+            //             'motherduck_token': MOTHERDUCK_TOKEN,
+            //         })
+            //         const connection = db.connect()
+            //         return (query: string) => {
+            //             return new Promise((resolve, reject) => {
+            //                 connection.all(`SET home_directory='/tmp'; ${query}`, (err: any, res: any) => {
+            //                     if (err) reject(err);
+            //                     resolve(res);
+            //                 })
+            //             })
+            //         }
+            //     })
 
             // const instance = await DuckDBInstance.create('src/lib/server/data/duck_kindle_audible.db');
             // const db = await instance.connect();
-            return _query;
+            return db;
         },
         destroy: async (con) => {
             // await con.close();
@@ -53,8 +58,8 @@ BigInt.prototype.toJSON = function () { return Number(this) }
 export async function query(sql: string) {
     const db = await pool.acquire();
     try {
-        // const result = await db(sql);
-        return {};
+        const result = await db.all(sql);
+        return result;
     } catch (e) {
         console.log(e)
     } finally {
